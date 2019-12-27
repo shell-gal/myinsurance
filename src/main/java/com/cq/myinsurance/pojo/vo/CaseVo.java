94e7748ac8162fc44b21c1480999a3b50187d106
Cheirmin <34155913+Cheirmin@users.noreply.github.com>
2019-12-27 16:22:34 +0800

    基本理算处理显示--无分页
    1、理算处理跳转
    2、里算出处理数据查询
    3、新建CaseVo的vo层

---
+++ src/main/java/com/cq/myinsurance/pojo/vo/CaseVo.java
@@ -0,0 +1,119 @@
+package com.cq.myinsurance.pojo.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Message:
+ * @Author：Cheirmin
+ * @Date: 2019/12/27 11:07
+ * @Version 1.0
+ */
+public class CaseVo implements Serializable {
+    //报案编号
+    private Integer caseId;
+    //报案人
+    private String reporterName;
+    //车牌号
+    private String carNumber;
+    //联系电话
+    private String reporterPhone;
+    //报案时间
+    private Date dangerDate;
+    //出险地点
+    private String dangerAddress;
+    //出险原因
+    private String dangerCause;
+    //事故类型
+    private String dangerType;
+    //案件状态
+    private String caseStatus;
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public Integer getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(Integer caseId) {
+        this.caseId = caseId;
+    }
+
+    public String getReporterName() {
+        return reporterName;
+    }
+
+    public void setReporterName(String reporterName) {
+        this.reporterName = reporterName;
+    }
+
+    public String getReporterPhone() {
+        return reporterPhone;
+    }
+
+    public void setReporterPhone(String reporterPhone) {
+        this.reporterPhone = reporterPhone;
+    }
+
+    public Date getDangerDate() {
+        return dangerDate;
+    }
+
+    public void setDangerDate(Date dangerDate) {
+        this.dangerDate = dangerDate;
+    }
+
+    public String getDangerAddress() {
+        return dangerAddress;
+    }
+
+    public void setDangerAddress(String province,String city,String dangerAddress) {
+        province = province.indexOf("省")>0?province:province.concat("省");
+        this.dangerAddress = province.concat(city).concat(dangerAddress);
+    }
+
+    public String getDangerCause() {
+        return dangerCause;
+    }
+
+    public void setDangerCause(String dangerCause) {
+        this.dangerCause = dangerCause;
+    }
+
+    public String getDangerType() {
+        return dangerType;
+    }
+
+    public void setDangerType(String dangerType) {
+        this.dangerType = dangerType;
+    }
+
+    public String getCaseStatus() {
+        return caseStatus;
+    }
+
+    public void setCaseStatus(String caseStatus) {
+        this.caseStatus = caseStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "CaseVo{" +
+                "caseId=" + caseId +
+                ", reporterName='" + reporterName + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                ", reporterPhone='" + reporterPhone + '\'' +
+                ", dangerDate=" + dangerDate +
+                ", dangerAddress='" + dangerAddress + '\'' +
+                ", dangerCause='" + dangerCause + '\'' +
+                ", dangerType='" + dangerType + '\'' +
+                ", caseStatus='" + caseStatus + '\'' +
+                '}';
+    }
+}
